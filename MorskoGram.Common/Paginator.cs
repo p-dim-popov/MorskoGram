d988ab724fc17dc4903fe26a624347e06143a8d0
Petar Popov <p.dim.popov@gmail.com>
2021-02-28 20:56:43 +0200

    Implement search by hashtag

---
+++ MorskoGram.Common/Paginator.cs
@@ -0,0 +1,12 @@
+﻿namespace MorskoGram.Common
+{
+    using System;
+
+    public static class Paginator
+    {
+        public static (int Skip, int Take) GetPagination(int page, int itemsPerPage)
+        {
+            return ((Math.Max(1, page) - 1) * itemsPerPage, itemsPerPage);
+        }
+    }
+}

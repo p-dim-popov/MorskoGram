9b151aa7f78492d7c83e3824ccdcf01f7b8396dd
Petar Popov <p.dim.popov@gmail.com>
2021-02-28 11:10:43 +0200

    Style UserPage, display correct follow button on logged users

--- MorskoGram.Web.API/ClientApp/src/components/users/pages/User/UserPage.jsx
+++ MorskoGram.Web.API/ClientApp/src/components/users/pages/User/UserPage.jsx
@@ -1,42 +1,75 @@
 ﻿import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router';
-import {Col, Row} from 'reactstrap';
+import {
+    Button, Col, Row,
+} from 'reactstrap';
 import {FeedPage} from '../../../posts/pages';
-import {getAsync} from '../../../../utils/fetcher';
+import {getAsync, postAsync} from '../../../../utils/fetcher';
 import {UserViewModel} from '../../../../models/users';
 import {USERS} from '../../../../constants/endpoints';
 import {restManager} from '../../../../utils/restManager';
 import {FollowsPopover} from '../../FollowsPopover';
 import {FollowViewModel, FollowTypes} from '../../../../models/follows';
+import authService from '../../../api-authorization/AuthorizeService';
+
+import style from './style.module.css';
 
 export const UserPage = React.memo(function UserPage() {
     const {id: userId} = useParams();
     const [user, setUser] = useState(new UserViewModel());
+    const [loggedUser, setLoggedUser] = useState({});
+
+    const getUserAsync = () => getAsync(UserViewModel)(`${USERS}/${userId}`)
+        .then(setUser)
+        .catch(restManager);
+
     useEffect(() => {
-        getAsync(UserViewModel)(`${USERS}/${userId}`)
-            .then(setUser)
-            .catch(restManager);
+        getUserAsync();
     }, [userId]);
+
+    useEffect(() => {
+        authService.getUser()
+            .then(setLoggedUser);
+    }, []);
+
+    const toggleFollow = () => {
+        postAsync(null)(`${USERS}/follow/${userId}`)
+            .then(getUserAsync);
+    };
 
     return (
         <>
-            <Row>
-                <Col>{user.email}</Col>
-                <Col>
-                    <FollowsPopover
-                        title="Followers"
-                        dataSource={user.followers.map((x) => new FollowViewModel(x))}
-                        type={FollowTypes.followed}
-                    />
-                </Col>
-                <Col>
-                    <FollowsPopover
-                        title="Followings"
-                        dataSource={user.followings.map((x) => new FollowViewModel(x))}
-                        type={FollowTypes.follower}
-                    />
-                </Col>
-            </Row>
+            <div className={style.headerRow}>
+                <Row xs={1} sm={1} md={4}>
+                    <Col>
+                        <h5>
+                            {user.email}
+                        </h5>
+                    </Col>
+                    <Col>
+                        <FollowsPopover
+                            title="Followers"
+                            dataSource={user.followers.map((x) => new FollowViewModel(x))}
+                            type={FollowTypes.followed}
+                        />
+                    </Col>
+                    <Col>
+                        <FollowsPopover
+                            title="Followings"
+                            dataSource={user.followings.map((x) => new FollowViewModel(x))}
+                            type={FollowTypes.follower}
+                        />
+                    </Col>
+                    {loggedUser?.sub !== userId && (
+                        <Col>
+                            {user.followers.some((x) => x.followerId === loggedUser.sub)
+                                ? (<Button onClick={toggleFollow} color="danger">Unfollow</Button>)
+                                : (<Button onClick={toggleFollow} color="primary">Follow</Button>)}
+                        </Col>
+                    )}
+                </Row>
+            </div>
+            <hr/>
             <FeedPage userId={userId}/>
         </>
     );
